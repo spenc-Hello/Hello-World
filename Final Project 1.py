2dd32df3f5350a37bae78dabc06127e3a6f0e89f
Spencer Brady <sebrady@cpp.edu>
2020-08-04 10:00:16 -0700

    Intial Commit

---
+++ Final Project 1.py
@@ -0,0 +1,38 @@
+def initList(inFile):
+    l=[]
+    for line in inFile: 
+
+        newLine=line.rstrip('\n') 
+        row=newLine.split(',') 
+        for temperature in row:
+            l.append(int(temperature))
+    return l
+
+def getSize(aList):
+  
+    return len(aList)
+
+def getHighest(aList):
+
+    aList.sort()
+  
+    return aList[len(aList)-1]
+
+def getLowest(aList):
+    return min(aList)
+
+def getAvearge(aList):
+    average=sum(aList)/len(aList)
+    return average
+
+def main():
+    fileName=input("enter file name: ")
+    inFile=open(fileName,"r")
+    aList=initList(inFile)
+
+    print("size: ",getSize(aList))
+    print("highest: ",getHighest(aList))
+    print("lowest: ",getLowest(aList))
+    print("average: ",getAvearge(aList))
+
+main()
